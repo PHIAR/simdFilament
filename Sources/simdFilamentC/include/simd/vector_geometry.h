103cb3ab844b2cbb6da7cb13e91aec72f49a2e68
Jim Zin <jim.zin@phiar.net>
2020-04-13 00:09:56 -0700

    Use custom simple dot product implementation

--- Sources/simdFilamentC/include/simd/vector_geometry.h
+++ Sources/simdFilamentC/include/simd/vector_geometry.h
@@ -7,31 +7,28 @@ extern "C"
 {
 #endif
 
-    float SIMD_ATTRIBUTES
-    simd_dot2(simd_float2 a, simd_float2 b);
-
-    float SIMD_ATTRIBUTES
-    simd_dot3(simd_float3 a, simd_float3 b);
-
-    float SIMD_ATTRIBUTES
-    simd_dot4(simd_float4 a, simd_float4 b);
-
     float SIMD_OVERLOADABLE
     simd_dot(simd_float2 a, simd_float2 b)
     {
-        return simd_dot2(a, b);
+        simd_float2 elements = a * b;
+
+        return elements.x + elements.y;
     }
 
     float SIMD_OVERLOADABLE
     simd_dot(simd_float3 a, simd_float3 b)
     {
-        return simd_dot3(a, b);
+        simd_float3 elements = a * b;
+
+        return elements.x + elements.y + elements.z;
     }
 
     float SIMD_OVERLOADABLE
     simd_dot(simd_float4 a, simd_float4 b)
     {
-        return simd_dot4(a, b);
+        simd_float4 elements = a * b;
+
+        return (elements.x + elements.y) + (elements.z + elements.w);
     }
 
     simd_float3 SIMD_ATTRIBUTES
--- Sources/simdFilamentC/simd.cpp
+++ Sources/simdFilamentC/simd.cpp
@@ -6,38 +6,6 @@
 
 using namespace filament::math;
 
-#pragma mark Dot Product
-
-extern "C"
-{
-    float
-    simd_dot2(simd_float2 a, simd_float2 b)
-    {
-        auto const &_a = *reinterpret_cast<float2 const *>(&a);
-        auto const &_b = *reinterpret_cast<float2 const *>(&b);
-
-        return dot(_a, _b);
-    }
-
-    float
-    simd_dot3(simd_float3 a, simd_float3 b)
-    {
-        auto const &_a = *reinterpret_cast<float3 const *>(&a);
-        auto const &_b = *reinterpret_cast<float3 const *>(&b);
-
-        return dot(_a, _b);
-    }
-
-    float
-    simd_dot4(simd_float4 a, simd_float4 b)
-    {
-        auto const &_a = *reinterpret_cast<float4 const *>(&a);
-        auto const &_b = *reinterpret_cast<float4 const *>(&b);
-
-        return dot(_a, _b);
-    }
-}
-
 #pragma mark Cross Product
 
 extern "C"
